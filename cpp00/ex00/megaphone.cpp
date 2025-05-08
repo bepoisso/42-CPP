9af21a4b8d88c59078d6ea3ca027fee02f0dc397
bepoisso <github@bpoisson.fr>
2025-05-08 16:00:34 +0200

    Add ex00

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -3,10 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bepoisso <bepoisso@student.42perpignan.    +#+  +:+       +#+        */
+/*   By: bepoisso <bepoisso@student.perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 16:07:49 by bepoisso          #+#    #+#             */
-/*   Updated: 2025/05/07 16:07:50 by bepoisso         ###   ########.fr       */
+/*   Updated: 2025/05/08 15:12:21 by bepoisso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+
+int main(int ac, char **av)
+{
+	if (ac >= 3)
+		return (std::cerr << "error: bad arguments" << std::endl, 1);
+	if (ac == 1)
+		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl, 0);
+	for (int i = 0; av[1][i]; i++) {
+		std::cout << (char)toupper(av[1][i]);
+	}
+	return (std::cout << std::endl, 0);
+}
