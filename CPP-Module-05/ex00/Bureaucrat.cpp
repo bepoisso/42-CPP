9499e6249cf03033ca7762c18a909a71f16ccc16
bepoisso <github@bpoisson.fr>
2025-06-04 15:31:50 +0200

    ADD M05EX00

---
+++ CPP-Module-05/ex00/Bureaucrat.cpp
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bepoisso <bepoisso@student.perpignan.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/04 14:04:54 by bepoisso          #+#    #+#             */
+/*   Updated: 2025/06/04 15:25:41 by bepoisso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+/* Exception */
+const char * Bureaucrat::gradeTooHighException::what() const throw() {
+	return "error: grade to high";
+}
+
+const char * Bureaucrat::gradeTooLowException::what() const throw() {
+	return "error: grade to low";
+}
+
+
+
+Bureaucrat::Bureaucrat(void) : _name("Visitor"), _grade(150){
+}
+
+Bureaucrat::Bureaucrat(std::string newName, int newGrade) : _name(newName), _grade(newGrade){
+	if (_grade < 1)
+		throw gradeTooHighException();
+	else if (_grade > 150)
+		throw gradeTooLowException();
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& src) : _name(src._name), _grade(src._grade) {
+	*this = src;
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& rhs) {
+	if (&rhs != this) {
+		_name = rhs._name;
+		_grade = rhs._grade;
+	}
+	return *this;
+}
+
+Bureaucrat::~Bureaucrat(void) {
+}
+
+/* Getter / Setter */
+std::string const Bureaucrat::getName(void) const {
+	return _name;
+}
+
+int Bureaucrat::getGrade(void) const {
+	return _grade;
+}
+
+/* Function */
+void	Bureaucrat::promotGrade(void) {
+	if (_grade - 1 < 1)
+		throw gradeTooHighException();
+	_grade--;
+}
+void	Bureaucrat::demotGrade(void) {
+	if (_grade + 1 > 150)
+		throw gradeTooLowException();
+	_grade++;
+}
+
+std::ostream & operator<<(std::ostream & out, Bureaucrat const & value) {
+	out << value.getName() << ", bureaucrat grade " << value.getGrade() << std::endl;
+	return (out);
+}
